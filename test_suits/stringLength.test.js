32972c28826e4885f31f8d70ff7ba4efac64fc48
jodrack <drack.sir01@gmail.com>
2022-05-02 19:56:34 +0200

    Check if the string length is between 1 and 10

--- test_suits/stringLength.test.js
+++ test_suits/stringLength.test.js
@@ -8,4 +8,8 @@ describe('String Length', () => {
   it('should return 10', () => {
     expect(stringLength('javascript')).toBe(10);
   });
+
+  it('should not be less than 1 and greater than 10', () => {
+    expect(stringLength('')).toBeFalsy();
+  });
 });
