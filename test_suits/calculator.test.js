d62ffab45ad3f4a7d66d1d03f994574186c9bea4
jodrack <drack.sir01@gmail.com>
2022-05-02 21:53:10 +0200

    Add test cases for the subtract method

--- test_suits/calculator.test.js
+++ test_suits/calculator.test.js
@@ -13,3 +13,17 @@ describe('Addition', () => {
     expect(add(-2, -1)).toBeLessThan(0);
   });
 });
+
+describe('Subtraction', () => {
+  it('Should return 1', () => {
+    expect(subtract(3, 2)).toEqual(1);
+  });
+
+  it('should return 0 when adding identical numbers', () => {
+    expect(subtract(3, 3)).toEqual(0);
+  });
+
+  it('shoud return a negative number when substracting 2 negative numbers', () => {
+    expect(subtract(-2, -1)).toBeLessThan(0);
+  });
+});
