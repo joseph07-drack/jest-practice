220f704467f6e442d21b2964b53a0650a910a095
jodrack <drack.sir01@gmail.com>
2022-05-02 21:08:28 +0200

    Export all calculator methods

--- calculator.js
+++ calculator.js
@@ -1,12 +1,12 @@
-const add = (num1, num2) => {
+export const add = (num1, num2) => {
   return num1 + num2;
 };
 
-const subtract = (num1, num2) => {
+export const subtract = (num1, num2) => {
   return num1 - num2;
 };
 
-const divide = (num1, num2) => {
+export const divide = (num1, num2) => {
   // return undefined if num2 is less than 0
   return num2 < 1 ? undefined : num1 / num2;
 };
