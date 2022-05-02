039d399d5bb94d4e285bcaae48b848c4194a7368
jodrack <drack.sir01@gmail.com>
2022-05-02 21:16:09 +0200

    Use commonJS for named imports

--- calculator.js
+++ calculator.js
@@ -1,16 +1,21 @@
-export const add = (num1, num2) => {
+const add = (num1, num2) => {
   return num1 + num2;
 };
 
-export const subtract = (num1, num2) => {
+const subtract = (num1, num2) => {
   return num1 - num2;
 };
 
-export const divide = (num1, num2) => {
+const divide = (num1, num2) => {
   // return undefined if num2 is less than 0
   return num2 < 1 ? undefined : num1 / num2;
 };
 
 const multiply = (num1, num2) => {
   return num1 * num2;
+};
+
+module.exports = {
+  add,
+  subtract,
 };
