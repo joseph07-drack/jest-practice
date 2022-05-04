c1153ebeaa2600b02e11aca13e2bb1e716a8fbd1
jodrack <drack.sir01@gmail.com>
2022-05-04 16:03:40 +0200

    Fix a slight issue

--- calculator.js
+++ calculator.js
@@ -1,23 +1,20 @@
-const add = (num1, num2) => {
-  return num1 + num2;
-};
+class Calculator {
+  static add = (num1, num2) => {
+    return num1 + num2;
+  };
 
-const subtract = (num1, num2) => {
-  return num1 - num2;
-};
+  static subtract = (num1, num2) => {
+    return num1 - num2;
+  };
 
-const divide = (num1, num2) => {
-  if (num2 === 0) return undefined;
-  return num1 / num2;
-};
+  static divide = (num1, num2) => {
+    if (num2 === 0) return undefined;
+    return num1 / num2;
+  };
 
-const multiply = (num1, num2) => {
-  return num1 * num2;
-};
+  static multiply = (num1, num2) => {
+    return num1 * num2;
+  };
+}
 
-module.exports = {
-  add,
-  subtract,
-  divide,
-  multiply,
-};
+module.exports = Calculator;
--- test_suits/calculator.test.js
+++ test_suits/calculator.test.js
@@ -1,61 +1,61 @@
-const { add, subtract, divide, multiply } = require('../calculator');
+const Calculator = require('../calculator');
 
 describe('Addition', () => {
   it('should return 5', () => {
-    expect(add(2, 3)).toEqual(5);
+    expect(Calculator.add(2, 3)).toEqual(5);
   });
 
   it('shoud return a positve number when adding 2 positives numbers', () => {
-    expect(add(1, 2)).toBeGreaterThan(0);
+    expect(Calculator.add(1, 2)).toBeGreaterThan(0);
   });
 
   it('shoud return a negative number when adding 2 negative numbers', () => {
-    expect(add(-2, -1)).toBeLessThan(0);
+    expect(Calculator.add(-2, -1)).toBeLessThan(0);
   });
 });
 
 describe('Subtraction', () => {
   it('Should return 1', () => {
-    expect(subtract(3, 2)).toEqual(1);
+    expect(Calculator.subtract(3, 2)).toEqual(1);
   });
 
   it('should return 0 when adding identical numbers', () => {
-    expect(subtract(3, 3)).toEqual(0);
+    expect(Calculator.subtract(3, 3)).toEqual(0);
   });
 
   it('shoud return a negative number when substracting 2 negative numbers', () => {
-    expect(subtract(-2, -1)).toBeLessThan(0);
+    expect(Calculator.subtract(-2, -1)).toBeLessThan(0);
   });
 });
 
 describe('Division', () => {
   it('should return underfine if the 2nd number is 0', () => {
-    expect(divide(2, 0)).toBeFalsy();
+    expect(Calculator.divide(2, 0)).toBeFalsy();
   });
 
   it('should return 10', () => {
-    expect(divide(10, 2)).toEqual(5);
+    expect(Calculator.divide(10, 2)).toEqual(5);
   });
 
   it('should return 1 when dividing identical numbers', () => {
-    expect(divide(5, 5)).toEqual(1);
+    expect(Calculator.divide(5, 5)).toEqual(1);
   });
 });
 
 describe('Multiplication', () => {
   it('should return 10', () => {
-    expect(multiply(2, 5)).toEqual(10);
+    expect(Calculator.multiply(2, 5)).toEqual(10);
   });
 
   it('should return 0 if any zero present', () => {
-    expect(multiply(4, 0)).toEqual(0);
+    expect(Calculator.multiply(4, 0)).toEqual(0);
   });
 
   it('shoud return a positive number if both numbers are negative', () => {
-    expect(multiply(-3, -3)).toEqual(9);
+    expect(Calculator.multiply(-3, -3)).toEqual(9);
   });
 
   it('shoud return a negative number if one number is negative', () => {
-    expect(multiply(-3, 3)).toEqual(-9);
+    expect(Calculator.multiply(-3, 3)).toEqual(-9);
   });
 });
